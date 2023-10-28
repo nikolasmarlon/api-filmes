381c1e7197afa9e9d965c5265615c133f389ef9c
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-10-28 20:08:59 -0300

    request.params

--- src/server.js
+++ src/server.js
@@ -3,6 +3,18 @@ const express = require('express')
 
 const app = express() // Inicializando express
 
-const PORTA = 3333
 
+app.get("/mensagem/:id/:user", (request, response) => {
+    response.send(`Olá, mundo! ${request.params.id}, para o usuário : ${request.params.user}`)
+})
+
+
+
+
+
+
+
+
+
+const PORTA = 3333
 app.listen(PORTA, () => console.log(`Porta: ${PORTA}`))
