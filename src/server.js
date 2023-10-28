8dd80765cdc9d7fc7082bf7afabf4a29fa84a1e0
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-10-28 20:29:16 -0300

    Query Params

--- src/server.js
+++ src/server.js
@@ -8,10 +8,16 @@ app.get("/mensagem/:id/:user", (request, response) => {
 
     const {id, user} = request.params
 
-    response.send(`Olá, mundo! ${id}, para o usuário : ${user}`) // Route params
+    response.send(`Olá, mundo! ${id}, para o usuário : ${user}`) // Route params === são obrigatórios
 })
 
 
+
+app.get("/users", (requisicao, resposta) => {
+    const { pagina, limite} = requisicao.query
+
+    resposta.send(`Página: ${pagina}, limite: ${limite}`) //Query Params === opcional exemplo rota no navegador http://localhost:3333/users?pagina=4&&limite=10
+})
 
 
 
