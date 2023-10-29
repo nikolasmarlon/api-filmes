b32f1b911e3699e5bae53477f87eb5ad21478704
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-10-28 23:06:59 -0300

    Primeira forma de usar middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -5,9 +5,22 @@ const UsersController = require('../controllers/UsersController') // classe prec
 //Inicializando Router()
 const usersRoutes = Router()
 
+
+//Duas formas de usar o middleware
+// 1ª Passar para cada rota para interceptar a requisição e a resposta e o next
+function meuMiddleware(requisicao, resposta, next){
+    console.log('voce passou pelo middleware')
+    console.log(requisicao.body)
+
+    if(!requisicao.body.isAdmin){
+        return resposta.json({ mensagem: "Usuário nao altorizado!"})
+    }
+    next()
+}
+
 const usersController = new UsersController()
 
 // A rota vai receber a requisição e a resposta e repassar para o controller
-usersRoutes.post("/",  usersController.create)
+usersRoutes.post("/", meuMiddleware,  usersController.create) // a rota tem o endereço, o middleware e o controller, ( antes do executar o controller, tudo vai passar pelo middleware)
 
 module.exports = usersRoutes
