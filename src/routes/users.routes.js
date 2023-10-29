d8ec10c03f4a04a947c2f60df193ee6c4591e56e
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-10-28 23:08:56 -0300

    segunda forma de usar o middleware

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -8,6 +8,7 @@ const usersRoutes = Router()
 
 //Duas formas de usar o middleware
 // 1ª Passar para cada rota para interceptar a requisição e a resposta e o next
+// 2ª Segunda forma é passando para todas as rotas 
 function meuMiddleware(requisicao, resposta, next){
     console.log('voce passou pelo middleware')
     console.log(requisicao.body)
@@ -18,9 +19,11 @@ function meuMiddleware(requisicao, resposta, next){
     next()
 }
 
+usersRoutes.use(meuMiddleware)
+
 const usersController = new UsersController()
 
 // A rota vai receber a requisição e a resposta e repassar para o controller
-usersRoutes.post("/", meuMiddleware,  usersController.create) // a rota tem o endereço, o middleware e o controller, ( antes do executar o controller, tudo vai passar pelo middleware)
+usersRoutes.post("/", usersController.create) // a rota tem o endereço, o middleware e o controller, ( antes do executar o controller, tudo vai passar pelo middleware)
 
 module.exports = usersRoutes
