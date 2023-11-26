253a975bdb41ed57b34d9b4004ab08812ad74b21
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-11-26 00:13:51 -0300

    Validando senha

--- src/controllers/SessionsController.js
+++ src/controllers/SessionsController.js
@@ -3,6 +3,8 @@ const knex = require('../database/knex')
 
 const AppError = require('../utils/AppError')
 
+const { compare } = require('bcryptjs')
+
 class SessionsController {
 
 
@@ -14,6 +16,12 @@ class SessionsController {
         const user = await knex("users").where({ email }).first()
 
         if(!user){
+            throw new AppError("E-mail e/ou senha incorreto", 401)
+        }
+
+        const passwordMatched = await compare(password, user.password)
+
+        if(!passwordMatched){
             throw new AppError("E-mail e/ou senha incorreto", 401)
         }
 
