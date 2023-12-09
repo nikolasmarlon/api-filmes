a3bc987c42809da27fc2fd443b45a08c08c994ac
Nikolas Marlon <40303291+nikolasmarlon@users.noreply.github.com>
2023-12-09 09:30:51 -0300

    Configuração do upload

---
+++ src/configs/upload.js
@@ -0,0 +1,27 @@
+const path  = require("path")
+const multer = require("multer")
+const crypto = require("crypto")
+
+const TMP_FOLDER = path.resolve(__dirname, "..", "..", "tmp") // endereço
+const UPLOADS_FOLDER = path.resolve(__dirname, "uploads") // endereço
+
+// Multer é a biblioteca para fazer o upload
+const MULTER = {
+    storage: multer.diskStorage({
+        destination: TMP_FOLDER,
+        filename(request, file, callback){
+            //evitar arquivos com nomes iguais
+            const fileHash = crypto.randomBytes(10).toString("hex")
+            const fileName = `${fileHash}-${file.originalname}`
+
+            return callback(null, fileName)
+        }
+    })
+}
+
+
+module.exports = {
+    TMP_FOLDER,
+    UPLOADS_FOLDER,
+    MULTER,
+}
